3f8d5934d335ee3d8c3fd0ccc8f387d8930cc371
GabrielFDJ <gabrielferreiradj2014@gmail.com>
2021-09-25 09:54:25 -0300

    métodos de contribuinte

---
+++ Atividades/impostoderenda/Contribuinte.java
@@ -0,0 +1,29 @@
+package impostoderenda;
+
+public abstract class Contribuinte {
+    
+    private String nome;
+    private double rendaBruta;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getRendaBruta() {
+        return rendaBruta;
+    }
+    public void setRendaBruta(double rendaBruta) {
+        this.rendaBruta = rendaBruta;
+    }
+
+    public abstract double calcularimposto();   
+    
+    @Override
+    public String toString() {
+        return "Contribuinte [nome=" + nome + ", rendaBruta=" + rendaBruta + "]";
+    }
+
+    
+}
