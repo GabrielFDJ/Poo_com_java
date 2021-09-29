53e9c8cbacc6b340aba11f73676b2339a50e6ba5
GabrielFDJ <gabrielferreiradj2014@gmail.com>
2021-09-29 10:24:30 -0300

    classe conta e seus métodos

---
+++ Atividades/heranca_banco/Conta.java
@@ -0,0 +1,45 @@
+package heranca_banco;
+
+public class Conta {
+	private int numero;
+	private double saldo;
+	
+	
+	public Conta(int numero, double saldo) {
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+
+	public void depositar(double valor) {
+		this.saldo = this.saldo+valor;
+	}
+
+	public void sacar(double valor) {
+		this.saldo=this.saldo-valor;
+		System.out.println("valor sacado : "+valor);		
+	}
+
+
+	public void atualizacao() {
+		
+	}
+}
