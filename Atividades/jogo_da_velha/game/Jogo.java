59052cd94c79a91463b82f35c205e76055b3a105
GabrielFDJ <gabrielferreiradj2014@gmail.com>
2021-10-06 10:09:52 -0300

    adicionando comparações e métodos a jogo da velha

--- Atividades/jogo_da_velha/game/Jogo.java
+++ Atividades/jogo_da_velha/game/Jogo.java
@@ -1,25 +1,80 @@
 package game;
 
 public class Jogo {
-	private boolean status = true;
+	private int status = 1;
 	private int contador = 0;
 	private String[] tabuleiro = new String[9];
 	private String Vencedor = " "; 
 	
 	public Jogo() {}
 	
+	public void inicializar_tablueiro() {
+		for(int i=0; i<9; i++) {
+			tabuleiro[i] = "z";			
+		}
+			
+	}
+	
 	public void preenche_tabuleiro(String verific, int index) {
 		tabuleiro[index] = verific;
 		contador++;	
 	}
 	
-	public String verifica_status(){
-		if(status = true && tabuleiro[0].equals(tabuleiro[1])&& tabuleiro[0].equals(tabuleiro[2])) {
-			status = false;			
+	public String verifica_status(){		
+		System.out.println(this.status);
+		for(int i=0; i<9; i++) {			
+			System.out.println(tabuleiro[i]);
+		}
+		
+		if(this.status == 1 && tabuleiro[0] != "z" && tabuleiro[0].equals(tabuleiro[1]) && tabuleiro[0].equals(tabuleiro[2])) {
+			this.status = 0;			
 			Vencedor = test_vencedor(tabuleiro[0]);
 			System.out.println("entrou - " + contador);
 		}
-		if(contador == 9)
+				
+		else if(this.status == 1 && tabuleiro[0] != "z" && tabuleiro[0].equals(tabuleiro[3]) && tabuleiro[0].equals(tabuleiro[6])) {
+			this.status = 0;			
+			Vencedor = test_vencedor(tabuleiro[0]);
+			System.out.println("entrou - " + contador);
+		} 
+		
+		else if(this.status == 1 && tabuleiro[0] != "z" && tabuleiro[0].equals(tabuleiro[4]) && tabuleiro[0].equals(tabuleiro[8])) {
+			this.status = 0;			
+			Vencedor = test_vencedor(tabuleiro[0]);
+			System.out.println("entrou - " + contador);
+		} 
+		
+		else if(this.status == 1 && tabuleiro[1] != "z" && tabuleiro[1].equals(tabuleiro[4]) && tabuleiro[1].equals(tabuleiro[7])) {
+			this.status = 0;			
+			Vencedor = test_vencedor(tabuleiro[1]);
+			System.out.println("entrou - " + contador);
+		} 
+			
+		else if(this.status == 1 && tabuleiro[2] != "z" && tabuleiro[2].equals(tabuleiro[4]) && tabuleiro[2].equals(tabuleiro[6])) {
+			this.status = 0;			
+			Vencedor = test_vencedor(tabuleiro[2]);
+			System.out.println("entrou - " + contador);
+		} 
+		
+		else if(this.status == 1 && tabuleiro[2] != "z" && tabuleiro[2].equals(tabuleiro[5]) && tabuleiro[2].equals(tabuleiro[8])) {
+			this.status = 0;			
+			Vencedor = test_vencedor(tabuleiro[2]);
+			System.out.println("entrou - " + contador);
+		}
+		
+		else if(this.status == 1 && tabuleiro[3] != "z" && tabuleiro[3].equals(tabuleiro[4]) && tabuleiro[3].equals(tabuleiro[5])) {
+			this.status = 0;			
+			Vencedor = test_vencedor(tabuleiro[3]);
+			System.out.println("entrou - " + contador);
+		}
+		
+		else if(this.status == 1 && tabuleiro[6] != "z" && tabuleiro[6].equals(tabuleiro[7]) && tabuleiro[6].equals(tabuleiro[8])) {
+			this.status = 0;			
+			Vencedor = test_vencedor(tabuleiro[6]);
+			System.out.println("entrou - " + contador);
+		}
+		
+		if(this.status == 1 && contador == 9)
 			Vencedor = "Empate";
 		return Vencedor;
 	}
@@ -34,6 +89,16 @@ public class Jogo {
 			
 		return vencedor;
 	}	
+	
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}	
+	
+	
 	
 	
 }
